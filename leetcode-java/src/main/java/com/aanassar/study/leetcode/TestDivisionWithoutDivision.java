63dade642c6e959ac9e1dc5116361cf35de0c186
Tony Nassar <tony.nassar@appian.com>
2017-11-22 09:44:58 -0500

    Adding Git boilerplate

--- leetcode-java/src/main/java/com/aanassar/study/leetcode/TestDivisionWithoutDivision.java
+++ leetcode-java/src/main/java/com/aanassar/study/leetcode/TestDivisionWithoutDivision.java
@@ -1,6 +1,8 @@
 package com.aanassar.study.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TestDivisionWithoutDivision {
 
@@ -12,19 +14,42 @@ public class TestDivisionWithoutDivision {
 			divisor = -divisor;
 		int result = 0;
 		while (dividend >= divisor) {
-			int shift = Integer.numberOfLeadingZeros(divisor) - Integer.numberOfLeadingZeros(dividend);
-			int subtrahend = 
+			int shift = 0;
+			while (dividend << shift < dividend) {
+				++shift;
+			}
 			dividend -= (divisor << shift);
+			result += 1 << shift;
 		}
 		return positive ? result : -result;
 	}
 
 	@Test
 	public void testZero() {
-		// assert divide(7, 1) == 7;
-		// assert divide(7, 2) == 3;
-		assert divide(7, -2) == -3;
+		assert divide(7, 1) == 7;
+		assert divide(7, 2) == 3;
 		assert divide(7, 8) == 0;
 		assert divide(10, 3) == 3;
+	}
+
+	@Test
+	public void testPositiveNegative() {
+		assert divide(7, -2) == -3;
+	}
+
+	@Test
+	public void testNegativePositive() {
+		assert divide(7, -2) == -3;
+	}
+
+	@Test
+	public void testNegativeNegative() {
+		assert divide(-7, -2) == 3;
+	}
+
+	@Test
+	public void testFromSite() {
+		int actual = divide(Integer.MIN_VALUE, -1);
+		assertThat(actual, equalTo(2147483647));
 	}
 }
