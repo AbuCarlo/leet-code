8b1f26985e3db5929ad75549a4667812d55a02b2
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-27 18:48:24 -0400

    WIP: closer, but still off-by-1 issues

--- leetcode-python/test_median_of_two_arrays.py
+++ leetcode-python/test_median_of_two_arrays.py
@@ -9,58 +9,39 @@ import statistics
 
 import hypothesis
 import hypothesis.strategies
+import pytest
 
 def median_of_two_sorted_arrays(l: list[int], r: list[int]) -> float:
     '''
     The assignment pretty clearly implies something like binary search.
     '''
-    odd_median = (len(l) + len(r)) % 2 == 1
-    # There *is* a median if 1 list is empty.
-    if not l:
-        if not r:
-            # Python's statistics.median() raises a similar error.
-            raise ValueError('There is no median for empty data.')
-        return r[len(r) // 2] if odd_median else sum(r[len(r) // 2:][:2]) / 2.0
-    if not r:
-        return [len(l) // 2] if odd_median else sum(l[len(l) // 2:][:2]) / 2.0
+    if not l and not r:
+        raise ValueError('There is no median for empty data.')
 
-    # This many value need to be on either side of the median.
+    # Initially, the window onto each array extends across the array. We need
+    # to shrink each window so that this many values are excluded on either
+    # side of the windows.
     target = (len(l) + len(r) - 1) // 2
+    odd_median = (len(l) + len(r)) % 2 == 1
 
-    l_left_limit = 0
-    r_left_limit = 0
+    left = 0
 
-    l_index = len(l) // 2
-    r_index = len(r) // 2
-
-    while l_index + r_index < target:
-        if l[l_index] < r[r_index]:
-            l_left_limit, l_index = l_index, (l_index + l_left_limit) // 2
-            assert l_index >= l_left_limit
+    while left < target:
+        left_midpoint = min(target - left, len(l)) // 2
+        right_midpoint = min(target - left, len(r)) // 2
+        if len(r) == 0 or l[left_midpoint] <= r[right_midpoint]:
+            left += left_midpoint + 1
+            l = l[left_midpoint + 1:]
         else:
-            r_left_limit, r_index = r_index, (r_index + r_left_limit) // 2
-            assert r_index >= r_left_limit
-    # All the value to the left of the indices would be in the first half
-    # of the merged array. If the total length is odd, the median has to
-    # be the lower value at the two indices. If even, the median is the
-    # average of the two lowest values still available.
-    print(f'l = {l}: {l_index}; r = {r}: {r_index}')
-    if (len(l) + len(r)) % 2 == 1:
-        # *All* values in l were eliminated.
-        if l_index == 0:
-            return r[r_index]
-        if r_index == 0:
-            return l[l_index]
-        # Those were the edge cases.
-        return min(l[l_index], r[r_index])
-    # Let's say that l[l_index] < r[r_index]. Then l[l_index] is 
-    # one value that's averaged into the median, and the other is
-    # either r[r_index] or, if lower, l[l_index + 1]. And so on.
-    # Let's just take advantage of the fact that Python will *not*
-    # give us an IndexError if we use the range accessors.
-    values_for_median = l[l_index:l_index + 2] + r[r_index:r_index + 2]
-    values_for_median.sort()
-    return sum(values_for_median[0:2]) / 2
+            left += right_midpoint + 1
+            r = r[right_midpoint + 1:]
+
+    if odd_median:
+        return min(l[:1] + r[:1])
+    smallest_values = l[:2] + r[:2]
+    smallest_values.sort()
+    return sum(smallest_values[:2]) / 2.0
+
 
 # pylint: disable=C0301
 @hypothesis.strategies.composite
@@ -83,5 +64,20 @@ def test_any_array(lr):
     '''
     l, r = lr
     expected = statistics.median(itertools.chain(l, r))
+    actual = median_of_two_sorted_arrays(l, r)
+    assert actual == expected
+
+samples = [
+    ([1, 3], [2], 2),
+    ([0, 1], [0], 0),
+    ([0, 1], [1], 1),
+    ([1, 1], [0], 1)
+]
+
+@pytest.mark.parametrize("l, r, expected", samples)
+def test_samples(l, r, expected):
+    '''
+    Sample and further test cases from Leetcode.
+    '''
     actual = median_of_two_sorted_arrays(l, r)
     assert actual == expected
