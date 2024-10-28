169f1907ed00e63f27b063e8e14ab515ba37e203
Tony Nassar <anthonyabunassar@gmail.com>
2024-10-28 09:45:54 -0400

    Solved boundary issues.

--- leetcode-python/test_median_of_two_arrays.py
+++ leetcode-python/test_median_of_two_arrays.py
@@ -18,25 +18,50 @@ def median_of_two_sorted_arrays(l: list[int], r: list[int]) -> float:
     if not l and not r:
         raise ValueError('There is no median for empty data.')
 
-    # Initially, the window onto each array extends across the array. We need
-    # to shrink each window so that this many values are excluded on either
-    # side of the windows.
+    # Once we've eliminated this many elements, the values
+    # for the median will be in the prefix of either or
+    # both array.
     target = (len(l) + len(r) - 1) // 2
-    odd_median = (len(l) + len(r)) % 2 == 1
+    is_median_odd = (len(l) + len(r)) % 2 == 1
 
-    left = 0
+    eliminated = 0
 
-    while left < target:
-        left_midpoint = min(target - left, len(l)) // 2
-        right_midpoint = min(target - left, len(r)) // 2
-        if len(r) == 0 or l[left_midpoint] <= r[right_midpoint]:
-            left += left_midpoint + 1
-            l = l[left_midpoint + 1:]
-        else:
-            left += right_midpoint + 1
-            r = r[right_midpoint + 1:]
+    while eliminated < target:
+        if len(l) == 0:
+            r = r[target - eliminated:]
+            break
+        if len(r) == 0:
+            l = l[target - eliminated:]
+            break
 
-    if odd_median:
+        # This the edge case: we can only consider the first elements
+        # of the two arrays.
+        if target - eliminated == 1:
+            if l[0] <= r[0]:
+                l = l[1:]
+            else:
+                r = r[1:]
+            break
+
+        left_selection = min((target - eliminated) // 2 - 1, len(l) - 1)
+        right_selection = min((target - eliminated) // 2 - 1, len(r) - 1)
+
+        assert left_selection + 1 + right_selection + 1 <= target - eliminated
+
+        if l[left_selection] <= r[right_selection]:
+            # 0-based Python indexing
+            eliminated += left_selection + 1
+            # The assertion above ensures that we can do this:
+            # we can eliminate both prefixes here.
+            if l[left_selection] == r[right_selection]:
+                eliminated += right_selection + 1
+                r = r[right_selection + 1:]
+            l = l[left_selection + 1:]
+        elif l[left_selection] > r[right_selection]:
+            eliminated += right_selection + 1
+            r = r[right_selection + 1:]
+
+    if is_median_odd:
         return min(l[:1] + r[:1])
     smallest_values = l[:2] + r[:2]
     smallest_values.sort()
@@ -71,7 +96,10 @@ samples = [
     ([1, 3], [2], 2),
     ([0, 1], [0], 0),
     ([0, 1], [1], 1),
-    ([1, 1], [0], 1)
+    ([1, 1], [0], 1),
+    # edge cases I've found
+    ([1, 2, 3], [1, 2, 3, 4], 2),
+    ([1, 2, 3], [1, 2, 3], 2)
 ]
 
 @pytest.mark.parametrize("l, r, expected", samples)
