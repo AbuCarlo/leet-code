cbfc21ec48f31eba122434d00fbdbbc93c818472
Tony Nassar <anthonyabunassar@gmail.com>
2024-11-07 16:01:45 -0500

    Some off-by-one problems; almost there.

--- leetcode-python/test_merge_sorted_arrays.py
+++ leetcode-python/test_merge_sorted_arrays.py
@@ -18,37 +18,50 @@ class Solution:
         # Store excess values from the first array in the second. We'll have to
         # do this at most n times, because there can only be n values too large
         # to insert before b[0]
-        i = 0
+        if not b:
+            return
         j = 0
         retrieve_at = 0
         store_at = 0
-        for i in range(m):
-            if j < n and a[i] <= b[j]:
-                continue
-            if j == n:
-                break
-            t = b[j]
-            j += 1
-            b[store_at] = a[i]
-            store_at += 1
-            a[i] = t
-        i = m
-        while i < m + n:
-            if j == n:
-                a[i] = b[retrieve_at]
-                retrieve_at += 1
-                if retrieve_at == store_at:
-                    retrieve_at = store_at = 0
-            elif retrieve_at == store_at or b[j] < b[retrieve_at]:
-                a[i] = b[j]
-                j += 1
+        for i in range(m + n):
+            # Something's in the buffer.
+            if retrieve_at != store_at:
+                if j < n:
+                    if b[retrieve_at] <= b[j]:
+                        if i < m:
+                            t = a[i]
+                        a[i] = b[retrieve_at]
+                        retrieve_at += 1
+                        if retrieve_at == store_at:
+                            retrieve_at = store_at = 0
+                        if i < m:
+                            b[store_at] = t
+                            store_at += 1
+                    elif i < m and a[i] <= b[j]:
+                        pass
+                    else:
+                        if i < m:
+                            t = a[i]
+                        a[i] = b[j]
+                        j += 1
+                        if i < m:
+                            b[store_at] = t
+                            store_at += 1
             else:
-                a[i] = b[retrieve_at]
-                retrieve_at += 1
-                if retrieve_at == store_at:
-                    retrieve_at = store_at = 0
-            i += 1
-            
+                # Values here can be overwritten.
+                if i >= m:
+                    a[i] = b[j]
+                    j += 1
+                # The buffer must be empty.
+                elif j >= n or a[i] <= b[j]:
+                    pass
+                else:
+                    t = a[i]
+                    a[i] = b[j]
+                    b[store_at] = t
+                    store_at += 1
+                    
+                
 
 
 
@@ -57,7 +70,9 @@ _SAMPLES = [
     ([], []),
     ([1], []),
     ([2, 0], [1]),
-    ([1,2,4,5,6,0], [3])
+    ([1,2,4,5,6,0], [3]),
+    ([4,5,6,0,0,0], [1, 2, 3]),
+    ([0,0,3,0,0,0,0,0,0], [-1,1,1,1,2,3])
 ]
 
 @pytest.mark.parametrize("l, r", _SAMPLES)
