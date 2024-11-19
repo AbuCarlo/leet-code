f30ad8c3ef76206ba07314813ef38a3a1cb08c6b
Tony Nassar <anthonyabunassar@gmail.com>
2024-11-19 07:32:39 -0500

    minimizedMaximum() solved.

--- leetcode-python/test_minimum_maximum.py
+++ leetcode-python/test_minimum_maximum.py
@@ -7,7 +7,40 @@ from typing import List
 
 import pytest
 
+def minimized_maximum(n: int, quantities: List[int]) -> int:
+    '''
+    Use binary search to find the optimal result, i.e.,
+    Where q = |quantities|, O(q * log q) implementation.
+    
+    :param n: number of buckets to put goods in
+    :param quantities: quantity of each item, which can't be mixed
+    '''
+    if len(quantities) > n:
+        raise ValueError('There are more quantities than buckets to put them in.')
+    # "upper" is the upper bound on a solution. In the worst case, we just
+    # keep all the quantities together, and leave some buckets empty.
+    # "lower" is the total of quantities, distributed evenly to the buckets.
+    # This solution isn't possible, since we can't mix products.
+    upper = max(quantities)
+    lower = math.ceil(sum(quantities) / n)
+    while upper - lower > 1:
+        mid = (upper + lower) // 2
+        if sum(math.ceil(q / mid) for q in quantities) > n:
+            lower = mid
+        else:
+            upper = mid
+
+    if sum(math.ceil(q / lower) for q in quantities) <= n:
+        return lower
+    return upper
+
+
 _SAMPLES = [
+    # edge cases
+    (3, [1, 100], 50),
+    (4, [2, 4], 2),
+    (3, [11, 17], 11),
+    (3, [2, 10], 5),
     # samples
     (6, [11,6], 3),
     (7, [15,10,10], 5),
@@ -15,35 +48,6 @@ _SAMPLES = [
     # test cases
     (22, [25,11,29,6,24,4,29,18,6,13,25,30], 13)
 ]
-
-def minimized_maximum(n: int, quantities: List[int]) -> int:
-    '''
-    O(|quantities|) implementation.
-    '''
-    quantity_ix = 0
-    bucket_index = 0
-    result = 0
-    quantities.sort()
-    current_sum = sum(quantities)
-    while bucket_index < n:
-        current_avg = math.ceil(current_sum / (n - bucket_index))
-        while quantities[quantity_ix] < current_avg:
-            result = max(result, quantities[quantity_ix])
-            current_sum -= quantities[quantity_ix]
-            bucket_index += 1
-            quantity_ix += 1
-        current_avg = math.ceil(current_sum / (n - bucket_index))
-        # Split up the next quantity.
-        buckets = math.ceil(quantities[quantity_ix] / (current_avg))
-        sizes = math.ceil(quantities[quantity_ix] / buckets)
-        result = max(result, sizes)
-
-        current_sum -= quantities[quantity_ix]
-        bucket_index += buckets
-        quantity_ix += 1
-
-    assert quantity_ix == len(quantities)
-    return result
 
 
 # pylint: disable=C0116
