3db4891ff571d6571c63700b6f66f5b6cb0355d2
Tony Nassar <anthonyabunassar@gmail.com>
2024-11-23 12:16:00 -0500

    Solved rotateTheBox() of rocks.

---
+++ leetcode-python/test_rotate_the_box.py
@@ -0,0 +1,46 @@
+'''
+https://leetcode.com/problems/rotating-the-box
+'''
+
+from typing import List
+
+
+# pylint: disable=C0103
+def rotateTheBox(box: List[List[str]]) -> List[List[str]]:
+    for l in box:
+        obstacle = len(l)
+        for i in range(len(l) - 1, -1, -1):
+            c = l[i]
+            if c == '*':
+                obstacle = i
+            elif c == '#':
+                # A stone in this position will not fall,
+                # and therefore become an obstacle.
+                if i == len(l) - 1:
+                    obstacle = i
+                else:
+                    # This stone comes to rest on the
+                    # previous obstacle, and becomes a new one.
+                    # The current cell is left empty.
+                    obstacle -= 1
+                    l[i], l[obstacle] = '.', '#'
+            else:
+                assert l[i] == '.'
+
+    # Now rotate the whole thing Pythonically.
+    # Tip of the hat to https://stackoverflow.com/a/8421412/476942
+    # This gives us tuples...
+    return list(zip(*reversed(box)))
+
+
+
+# sample = [["#",".","#"]]
+
+# print(rotateTheBox(sample))
+
+# sample_3 = [["#",".","*","."],
+#               ["#","#","*","."]]
+
+# print(rotateTheBox(sample_3))
+
+print(rotateTheBox([["#","#","*",".","*","."],["#","#","#","*",".","."],["#","#","#",".","#","."]]))
