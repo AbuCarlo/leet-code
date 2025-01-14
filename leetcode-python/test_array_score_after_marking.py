14d9aa68a10440860fad44e8688f228103d60560
Tony Nassar <anthonyabunassar@gmail.com>
2025-01-14 13:56:28 -0500

    Satisfying pylint

--- leetcode-python/test_array_score_after_marking.py
+++ leetcode-python/test_array_score_after_marking.py
@@ -1,8 +1,6 @@
 '''
 https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/
 
-|nums| <= 10^5, nums[i] < 10^6
-
 '''
 
 from typing import List
@@ -11,6 +9,11 @@ import pytest
 
 
 def find_score(nums: List[int]) -> int:
+    '''
+    See above. |nums| <= 10^5, nums[i] < 10^6. 
+    Time: O(n log n) to sort the values. A heap
+    would have been appropriate.
+    '''
     marked_locations = set()
     values_with_locations = [(v, l) for l, v in enumerate(nums)]
     values_with_locations.sort()
@@ -23,11 +26,11 @@ def find_score(nums: List[int]) -> int:
         marked_locations.add(l - 1)
         marked_locations.add(l + 1)
     return score
-        
-    
+
+
 _TEST_CASES = [
     ([2,1,3,4,5,2], 7)
-]  
+]
 
 # pylint: disable=C0116
 @pytest.mark.parametrize("nums, expected", _TEST_CASES)
