2d057b970f05b6fa4714adfef5266280dea46b62
Tony Nassar <anthonyabunassar@gmail.com>
2025-01-14 20:01:48 -0500

    I just discovered that duplicate values are allowed.

--- leetcode-python/test_missing_positive.py
+++ leetcode-python/test_missing_positive.py
@@ -6,6 +6,7 @@ https://leetcode.com/problems/first-missing-positive/
 
 from typing import List
 
+from hypothesis import given, settings, strategies, Verbosity
 import pytest
 
 def _partition(a: List[int], low: int, high: int, pivot_index: int) -> int:
@@ -54,7 +55,8 @@ def firstMissingPositive(a: List[int])->int:
     except ValueError:
         return 1
 
-    # Start with positive integers.
+    # Start with positive integers. Partition first so that all
+    # non-positive integers are on the left, and can be ignored.
     start = _partition(a, 0, len(a) -1, midpoint)
     end = len(a) - 1
 
@@ -63,22 +65,23 @@ def firstMissingPositive(a: List[int])->int:
         # based on the pivot value, it's not missing anything.
         # Try the other one.
         if a[midpoint] - a[start] > midpoint - start:
-            print(f'Missing value between a[{start}] = {a[start]} and a[{midpoint}] = {a[midpoint]}')
+            # print(f'Missing value between a[{start}] = {a[start]} and a[{midpoint}] = {a[midpoint]}')
+            if midpoint - start < 2:
+                return a[start] + 1
             end = midpoint - 1
         elif a[end] - a[midpoint] > end - midpoint:
-            print(f'Missing value between a[{midpoint}] = {a[midpoint]} and a[{end}] = {a[end]}')
+            # print(f'Missing value between a[{midpoint}] = {a[midpoint]} and a[{end}] = {a[end]}')
+            if end - midpoint < 2:
+                return a[midpoint] + 1
             start = midpoint + 1
         else:
             # Both partitions are the right size: no value is missing.
             return max(a) + 1
-        if end - start == 1:
-            return a[start] + 1
         midpoint = _partition(a, start, end, (start + end) // 2)
 
 
 _TEST_CASES = [
     # my own test cases, to observe behavior of partitioning algorithm
-    # TODO Shuffle these.
     ([1], 2),
     ([1, 2], 3),
     (list(range(2, 5)), 1),
@@ -87,7 +90,9 @@ _TEST_CASES = [
     # test cases from description
     ([1,2,0], 3),
     ([3,4,-1,1], 2),
-    ([7,8,9,11,12], 1)
+    ([7,8,9,11,12], 1),
+    # test case #118: yes, duplicates may appear in the array!
+    ([1, 3, 3], 2)
 ]
 
 
@@ -97,4 +102,22 @@ def test_samples(a: List[int], expected: int):
     Test samples and test cases from Leetcode.
     '''
     actual = firstMissingPositive(a)
+    assert actual == expected
+
+# pylint: disable=C0301
+@given(strategies.lists(strategies.integers(min_value=-5, max_value=50), min_size=1, max_size=10000))
+@settings(verbosity=Verbosity.verbose)
+def test_any_array(a: List[int]):
+    '''
+    Test for any array of integers. We constrain the range of integers so that the arrays
+    are more likely to contain a 1, and thus circumvent the implementation.
+    '''
+    actual = firstMissingPositive(a)
+    positives = set(n for n in a if n > 0)
+    expected = 1
+    while True:
+        if expected not in positives:
+            break
+        expected += 1
+
     assert actual == expected
