156478bda7ebefa6dd19b34a26cf3bde46d82bc7
Tony Nassar <anthonyabunassar@gmail.com>
2025-01-21 10:42:06 -0500

    Pass 57 test cases; still not there.

--- leetcode-python/test_remove_shortest_subarray.py
+++ leetcode-python/test_remove_shortest_subarray.py
@@ -6,6 +6,7 @@ such that the remaining elements in arr are non-decreasing."
 
 '''
 
+import sys
 from typing import List, Tuple
 
 import pytest
@@ -28,7 +29,24 @@ def findLengthOfShortestSubarray(arr: List[int]) -> int:
     part of a sort. The minimum and maximum such indices 
     are the first and last indices of the subarray to be removed.
     '''
-    shifted = find_shifted(arr)
+    if not arr:
+        return 0
+    if len(arr) == 1:
+        return 0
+    if arr[0] > arr[-1]:
+        # The subarray to be deleted must start at 0, or end at -1.
+        max_left = 1
+        for n in arr[1:]:
+            if n < arr[0]:
+                break
+            max_left += 1
+        max_right = 1
+        for n in reversed(arr[:-1]):
+            if n > arr[-1]:
+                break
+            max_right += 1
+        return min(len(arr) - max_left, len(arr) - max_right)
+    shifted = find_shifted([-sys.maxsize] + arr + [sys.maxsize])
     if not shifted:
         return 0
     return max(shifted) - min(shifted) + 1
@@ -41,13 +59,17 @@ _SAMPLES =[
     ([1, 2, 3], 0),
     # special cases
     ([7, 8, 9, 4, 5, 6, 0, 1, 2, 3], 6),
+    ([5, 1, 2, 3, 4, 5], 1),
+    ([0, 1, 2, 3, 4, 0], 1),
     # test case #20
     ([2,2,2,1,1,1], 3),
     # variations of test case #20
     ([2,2,1,1,1], 2),
     ([2,2,2,1,1], 2),
+    # test case #58
+    ([1,2,3,10,0,7,8,9], 2),
 ]
-
+ 
 # pylint: disable=C0116
 @pytest.mark.parametrize("a, expected", _SAMPLES)
 def test_samples(a, expected):
