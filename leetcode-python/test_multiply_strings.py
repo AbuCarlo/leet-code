0659a9300fa746bf99313ec5af7f07df18f2c428
Tony Nassar <anthonyabunassar@gmail.com>
2025-01-21 12:57:10 -0500

    Solved.

--- leetcode-python/test_multiply_strings.py
+++ leetcode-python/test_multiply_strings.py
@@ -6,23 +6,62 @@ https://leetcode.com/problems/multiply-strings/
 
 '''
 
+from itertools import zip_longest
 from typing import List
 
+import pytest
 
-def multiply_left(l: List[int], d: int) -> int:
+def add_internal(l: List[int], r: List[int]) -> List[int]:
+    # '123' and '51' become [1, 5, 0], [3, 2, 1]
+    z = zip_longest(reversed(l), reversed(r), fillvalue=0)
+    carry = 0
+    result = []
+    # Start with lowest-significance.
+    for ld, rd in z:
+        d = ld + rd + carry
+        if d > 9:
+            d, carry = d % 10, d // 10
+        else:
+            carry = 0
+        result.append(d)
+    if carry > 0:
+        result.append(carry)
+    result.reverse()
+    print(f'{l} + {r} = {result}')
+    return result
+
+def multiply_left(l: List[int], d: int) -> List[int]:
     if not l:
-        return 0
-    return 10 * multiply_left(l[:-1], d) + l[-1] * d
+        return []
+    greater = multiply_left(l[:-1], d)
+    blah = [l[-1] * d]
+    return add_internal(greater + [0], blah)
 
 def multiply_internal(num1: str, num2: str) -> str:
     l = [ord(d) - ord('0') for d in num1]
     r = [ord(d) - ord('0') for d in num2]
 
-    result = 0
-    position = 1
+    result = []
+    trailing = []
     for d in reversed(r):
-        result += position * multiply_left(l, r)
-        position *= 10
+        m = multiply_left(l, d) + trailing
+        result = add_internal(result, m)
+        trailing += [0]
+    while len(result) > 1 and result[0] == 0:
+        result = result[1:]
     return ''.join(chr(d + ord('0')) for d in result)
 
-print(multiply_internal('123', '123'))
+_SAMPLES = [
+    ('2', '3', '6'),
+    ('123', '456', "56088"),
+    ('9', '9', '81'),
+    ("9133", '0', '0')
+]
+
+@pytest.mark.parametrize("l, r, expected", _SAMPLES)
+def test_samples(l, r, expected):
+    '''
+    Sample and further test cases from Leetcode.
+    '''
+    actual = multiply_internal(l, r)
+    assert actual == expected
