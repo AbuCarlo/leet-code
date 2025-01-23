59a49c5b928ebb1340448c56e73f4ac968dbb782
Tony Nassar <anthonyabunassar@gmail.com>
2025-01-23 13:54:28 -0500

    Solved using stack.

--- leetcode-python/test_asteroids.py
+++ leetcode-python/test_asteroids.py
@@ -2,7 +2,6 @@
 https://leetcode.com/problems/asteroid-collision
 '''
 
-from math import copysign
 from typing import List
 
 import pytest
@@ -12,48 +11,30 @@ def asteroid_collision(asteroids: List[int]) -> List[int]:
     '''
     Iterate pairwise over the asteroids.
     '''
-    if not asteroids:
-        return []
-    while len(set(copysign(1, a) for a in asteroids)) > 1:
-        i = 0
-        l = []
-        while i < len(asteroids):
-            if i == len(asteroids) - 1:
-                l.append(asteroids[i])
-                break
-            # If this asteroid is moving leftward, there can't be a collision.
-            if copysign(1, asteroids[i]) < 0:
-                l.append(asteroids[i])
-                i += 1
-            # Is the next asteroid moving leftward?
-            elif copysign(1, asteroids[i + 1]) < 0:
-                if abs(asteroids[i]) >= abs(asteroids[i + 1]):
-                    # Both asteroids explode.
-                    if abs(asteroids[i]) == abs(asteroids[i + 1]):
-                        i += 2
-                    # The next asteroid explodes.
-                    else:
-                        l.append(asteroids[i])
-                        i += 2
-                # This asteroid explodes.
-                else:
-                    l.append(asteroids[i + 1])
-                    i += 2
-            # These two asteroids are both moving rightward.
+    result = []
+    for a in asteroids:
+        # Until we've dealt with a...
+        while a is not None:
+            # Both asteroids are moving in the same direction, or further apart.
+            if not result or result[-1] < 0 or a > 0:
+                result.append(a)
+                a = None
+            # Both asteroids explode.
+            elif a == -result[-1]:
+                result.pop()
+                a = None
+            # The current asteroid is larger.
+            elif a < -result[-1]:
+                # Continue iterating.
+                result.pop()
+            # The current asteroid explodes.
             else:
-                l.append(asteroids[i])
-                i += 1
-        # No asteroids blew up.
-        if len(asteroids) == len(l):
-            break
-        asteroids = l
-
-    return asteroids
-
+                a = None
+    return result
 
 _SAMPLES = [
     ([5, 10, -5], [5, 10]),
-    ( [8, -8], []),
+    ([8, -8], []),
     ([10, 2, -5], [10])
 ]
 
