543e04c1658e1d57e0bc61fa1d6647f53d84ab9e
Anthony Nassar <anthonyabunassar@gmail.com>
2025-01-30 13:51:54 -0500

    Two sample test cases passed.

--- leetcode-python/test_next_greater_element_ii.py
+++ leetcode-python/test_next_greater_element_ii.py
@@ -3,41 +3,37 @@ https://leetcode.com/problems/next-greater-element-ii/
 '''
 
 import itertools
-from collections import deque
 from typing import List
 
 import pytest
 
 def next_greater_element(nums: List[int]) -> List[int]:
     '''
-    Given a circular integer array nums (i.e., the next element
+    "Given a circular integer array nums (i.e., the next element
     of nums[nums.length - 1] is nums[0]),
     return the next greater number for every element in nums.
 
     The next greater number of a number x is the first greater 
     number to its traversing-order next in the array, which means 
     you could search circularly to find its next greater number. 
-    If it doesn't exist, return -1 for this number.
+    If it doesn't exist, return -1 for this number."
     '''
-    deq = deque()
-    result = []
+    stack = []
+    result = [-1] * (len(nums) * 2)
     # Pretend that the input array is circular.
-    for n in itertools.chain(nums, nums):
-        while deq and n > deq[0]:
-            deq.popleft()
-            result.append(n)
+    for j, n in enumerate(itertools.chain(nums, nums)):
+        while stack and stack[-1][1] < n:
+            i, m = stack.pop()
+            result[i] = n
         # This element has to wait.
-        deq.append(n)
-
-    while stack:
-        stack.pop()
-        result.append(-1)
+        stack.append((j, n))
 
     return result[:len(nums)]
 
 
 _TEST_CASES = [
-    ([1,2,1], [2,-1,2])
+    ([1,2,1], [2,-1,2]),
+    ([1,2,3,4,3], [2,3,4,-1,4])
 ]
 
 
