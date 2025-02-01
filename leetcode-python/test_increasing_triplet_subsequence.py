b3536bf49390483764d8549ba270d06b90d683bb
Tony Nassar <anthonyabunassar@gmail.com>
2025-02-01 15:27:34 -0500

    Added alternate solution to increasing triplet

--- leetcode-python/test_increasing_triplet_subsequence.py
+++ leetcode-python/test_increasing_triplet_subsequence.py
@@ -6,9 +6,27 @@ triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k].
 If no such indices exists, return false.
 '''
 
+import itertools
 from typing import List
 
 import hypothesis
+
+def increasing_triplet_iterable(nums: List[int]) -> bool:
+    '''
+    Simply use partial sums to calculate minima and maxima.
+    It's baffling as to why LeetCode should consider this
+    solution more memory-efficient, as I'm allocating two
+    lists.
+    '''
+    if not nums or len(nums) < 3:
+        return False
+    minima = list(itertools.accumulate(nums, min))
+    maxima = list(itertools.accumulate(reversed(nums), max))
+    maxima.reverse()
+    for j in range(1, len(nums) - 1):
+        if nums[j] > minima[j] and nums[j] < maxima[j]:
+            return True
+    return False
 
 
 def increasing_triplet(nums: List[int]) -> bool:
@@ -49,7 +67,7 @@ def test_any_array(nums):
     Compare the above solution to a naive O(n^2) implementation. This has been
     tested on LeetCode, and it works, though #45 of the 51 test cases times out.
     '''
-    actual = increasing_triplet(nums)
+    actual = increasing_triplet_iterable(nums)
     # The naive O(n^3) implementation on shorter inputs.
     expected = any(nums[i] < nums[j] < nums[k] for i in range(0, len(nums) - 2) for j in range(i + 1, len(nums)) for k in range(j + 1, len(nums)))
     assert actual == expected
