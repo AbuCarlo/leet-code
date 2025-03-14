2472e57947b8ce5e82d2fc349935fcf1de74cf6f
Tony Nassar <tony@molg.ai>
2025-03-14 17:05:25 -0400

    WIP on someone else's test  code.'

---
+++ leetcode-python/test_max_distinct.groovy
@@ -0,0 +1,17 @@
+
+input = 'aaba'
+
+def do_the_thing(s) {
+    if not s:
+        return 1
+    
+    counter = new HashMap<Integer,Integer>
+    limit = new HashSet<Character>(s.toChars()).size()
+    result = 0
+    start = 0
+    end = 0
+
+    return 0
+}
+
+print do_the_thing(input)
--- leetcode-python/test_max_distinct.py
+++ leetcode-python/test_max_distinct.py
@@ -1,45 +1,50 @@
 input = 'aaba'
 
 def do_the_thing(s) -> int:
-    if not s:
-        return 1
-    # Let's allow a length of 1, as a edge case.
+    # Let's allow lengths of 0 and 1, as edge cases.
     from collections import defaultdict
     counter = defaultdict(int)
     limit = len(set(s))
     result = 0
     start = 0
+    # Use exclusively, per Python convention.
     end = 0
 
-    def advance():
-        nonlocal start
-        nonlocal end
-        nonlocal counter
-        nonlocal limit
-        for c in s[start:]:
+    # The substring s[start:] satisfies the requirement, 
+    # as do any of its substrings.
+
+    while True:
+        while end < len(s):
+            c = s[end]
+            # We can't add this character to the current substring.
             if counter[c] == limit:
                 break
             counter[c] += 1
             end += 1
-
-    # The substring s[start:end] satisfies the requirement, 
-    # as do any of its substrings.
-    advance()
-    result += end - start + 1
-    while end < len(s):
+        assert(len(set(s[start:end]))) <= limit
+        # Every substring starting at start meets the requirement.
+        result += end - start
+        if end == len(s):
+            break
+        
         # The character s[end] was the one that exceeded the
         # the limit. We need to get rid of the first instance
         # of it in the current substring, i.e. we have to 
         # delete the prefix up to and including it.
-        while counter[s[end]] == limit:
+        counter[s[end]] += 1
+        while counter[s[end]] > limit:
             counter[s[start]] -= 1
             start += 1
-        # Now start adding characters again.
-        advance()
 
-        result += end - start
+    # When we broke from the loop, s[start:end + 1] was a maximal substring.
+    # Now we need to include all substrings beginning after start.
+    start += 1
+    while start < len(s):
+        result += len(s) - start - 1
 
     return result
 
 output = do_the_thing(input)
 print(output)
+
+print(do_the_thing('abcdefg'))
