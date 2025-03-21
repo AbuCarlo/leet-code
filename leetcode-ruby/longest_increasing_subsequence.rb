dc2c4e32732f8dbdaaded2b257c69bf42371cf18
Tony Nassar <tony@molg.ai>
2025-03-21 18:19:44 -0400

    Got it working except for equal values.

--- leetcode-ruby/longest_increasing_subsequence.rb
+++ leetcode-ruby/longest_increasing_subsequence.rb
@@ -5,36 +5,50 @@
 # https://github.com/tomykaira/rspec-parameterized
 # https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/TreeSet.html
 
-Tuple = Struct.new(:value, :index)
+require 'rspec-parameterized'
+require 'rspec'
 
-def longest_subsequence(a)
+def longest_increasing_subsequence(a)
   if a.length == 0
     return 0
   end
   # Sort the array indices by the values!
-  sorted_indices = (0...a.count).sort { |l, r| if a[l] == a[r] then l <=> r else a[l] <=> a[r] end }
+  sorted_indices = (0...a.count).sort { |l, r|
+    if a[l] == a[r]
+      l <=> r
+    else
+      a[l] <=> a[r]
+    end }
   found = [a[sorted_indices[0]]]
   prefix_lengths = { found[0] => 1 }
   sorted_indices[1..].map { |i| [i, a[i]] }.each do |i, n|
-    insertion = found.bsearch_index { |e| e >= n - 1 }
+    insertion = found.bsearch_index { |e| e >= i }
     if insertion.nil?
       prefix_lengths[i] = prefix_lengths[found.last] + 1
       found.push(i)
-    elsif a[found[insertion]] == n
-      prefix_lengths[i] = [(prefix_lengths[found[insertion - 1]] + 1), prefix_lengths[n]].max
+    elsif n == a[found[insertion]]
+
     else
       prefix_lengths[i] = (prefix_lengths[found[insertion - 1]] || 0) + 1
-      found.insert(insertion + 1, i)
+      found.insert(insertion, i)
     end
   end
   prefix_lengths.values.max
 end
 
-test_cases = {
-  [10, 9, 2, 5, 3, 7, 101, 18] => 4
-}
+describe "longest_increasing_subsequence" do
+  where(:a, :expected) do
+    [
+      [[10, 9, 2, 5, 3, 7, 101, 18], 4],
+      [[0, 1, 0, 3, 2, 3], 4],
+      [[7, 7, 7, 7, 7, 7, 7], 1]
+    ]
+  end
 
-test_cases.each do |a, _b|
-  actual = longest_subsequence(a)
-  print actual
+  with_them do
+    it "should produce expected answer" do
+      actual = longest_increasing_subsequence(a)
+      expect(actual).to eq(expected)
+    end
+  end
 end
