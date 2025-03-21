9fba91b07ba7a7d7cc10ccfeb63ec1a4893f2b82
Tony Nassar <anthonyabunassar@gmail.com>
2025-03-21 19:53:26 -0400

    Renamed test file correctly.

--- leetcode-ruby/spec/longest_increasing_subsequence_spec.rb
+++ leetcode-ruby/spec/longest_increasing_subsequence_spec.rb
@@ -1,52 +1,53 @@
 # frozen_string_literal: true
 
 # https://leetcode.com/problems/longest-increasing-subsequence/description/
+# https://ruby-doc.org/3.4.1/bsearch_rdoc.html
+# https://github.com/tomykaira/rspec-parameterized
+# https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/TreeSet.html
 
 require 'rspec-parameterized'
 require 'rspec'
 
-def longest_increasing_subsequence(numbers)
-  return 0 if numbers.empty?
+def longest_increasing_subsequence(a)
+  if a.length == 0
+    return 0
+  end
+  # Sort the array indices by the values!
+  sorted_indices = (0...a.count).sort { |l, r|
+    if a[l] == a[r]
+      l <=> r
+    else
+      a[l] <=> a[r]
+    end }
+  found = [a[sorted_indices[0]]]
+  prefix_lengths = { found[0] => 1 }
+  sorted_indices[1..].map { |i| [i, a[i]] }.each do |i, n|
+    insertion = found.bsearch_index { |e| e >= i }
+    if insertion.nil?
+      prefix_lengths[i] = prefix_lengths[found.last] + 1
+      found.push(i)
+    elsif n == a[found[insertion]]
 
-  lengths = { numbers.last => 1 }
-  starting_values = [numbers.last]
-
-  # In Python, use reverse iterator.
-  numbers[0..-2].reverse.each do |number|
-    tail_start = starting_values.bsearch_index { |i| i >= number }
-    if tail_start.nil?
-      # There is no larger value.
-      lengths[number] = 1
-      starting_values.push(number)
-    elsif starting_values[tail_start] > number
-      # What is the longest run to which we could prepend this value?
-      lengths[number] = lengths.slice(*starting_values[tail_start..]).values.max + 1
-      # Here's where a tree would be useful.
-      starting_values.insert(tail_start, number)
-    elsif tail_start < starting_values.count - 1
-      # This value has already been encountered. If it's also the largest,
-      # there's nothing to do, since this value is already at the end of a run.
-      new_length = lengths.slice(*starting_values[tail_start + 1..]).values.max + 1
-      lengths[number] = new_length if lengths[number] < new_length
+    else
+      prefix_lengths[i] = (prefix_lengths[found[insertion - 1]] || 0) + 1
+      found.insert(insertion, i)
     end
   end
-  lengths.values.max
+  prefix_lengths.values.max
 end
 
-describe 'known test cases' do
-  where(:numbers, :expected) do
+describe "longest_increasing_subsequence" do
+  where(:a, :expected) do
     [
       [[10, 9, 2, 5, 3, 7, 101, 18], 4],
       [[0, 1, 0, 3, 2, 3], 4],
-      [[7, 7, 7, 7, 7, 7, 7], 1],
-      # test case 30
-      [[1, 3, 6, 7, 9, 4, 10, 5, 6], 6]
+      [[7, 7, 7, 7, 7, 7, 7], 1]
     ]
   end
 
   with_them do
-    it 'should produce expected answer' do
-      actual = longest_increasing_subsequence(numbers)
+    it "should produce expected answer" do
+      actual = longest_increasing_subsequence(a)
       expect(actual).to eq(expected)
     end
   end
