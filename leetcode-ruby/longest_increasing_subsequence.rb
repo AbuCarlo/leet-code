17f3c4809d8d31b8e389824039d969c57e037a3f
Tony Nassar <tony@molg.ai>
2025-03-21 14:02:27 -0400

    WIP

--- leetcode-ruby/longest_increasing_subsequence.rb
+++ leetcode-ruby/longest_increasing_subsequence.rb
@@ -5,16 +5,18 @@
 Tuple = Struct.new(:value, :index)
 
 def longest_subsequence(a)
-  # TODO: Values first; if ==, then index.
+  if a.length == 0
+    return 0
+  end
+  # Sort the array indices by the values!
   sorted_indices = (0...a.count).sort { |l, r| if a[l] == a[r] then l <=> r else a[l] <=> a[r] end }
-  found = []
-  prefix_lengths = {}
-  sorted_indices.map { |i| [i, a[i] ]}.each do |i, n|
-    # What is the largest value less than t.value?
-    insertion = found.bsearch_index { |e| e >= n }
+  found = [a[sorted_indices[0]]]
+  prefix_lengths = { found[0] => 1 }
+  sorted_indices[1..].map { |i| [i, a[i] ]}.each do |i, n|
+    insertion = found.bsearch_index { |e| e >= n - 1 }
     if insertion.nil?
-      found.unshift(n)
-      prefix_lengths[n] = 1
+      prefix_lengths[n] = prefix_lengths[found.last] + 1
+      found.push(n)
     elsif found[insertion] == n
       prefix_lengths[n] = [(prefix_lengths[found[insertion - 1]] + 1), prefix_lengths[n]].max
     else
