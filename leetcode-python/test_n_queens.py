aadefb532e0161f722da23a6cf17eb4fff050697
Tony Nassar <tony@molg.ai>
2025-03-26 15:02:07 -0400

    Adding new file

---
+++ leetcode-python/test_n_queens.py
@@ -0,0 +1,110 @@
+'''
+https://leetcode.com/problems/n-queens
+'''
+
+import pytest
+
+def totalNQueensBitmasks(n: int) -> int:
+    '''
+    Recursive implementation using bitsets to represent 
+    already occupied positions.
+
+    :param n: the size of the board, i.e. n * n
+    '''
+    def internal_queens(row: int, columns: int, lefts: int, rights: int) -> int:
+        '''
+        :param row: the row in which we're trying to place a queen
+        :param columns: the set of columns already having a queen
+        :param rows: the set of rows etc.
+        :param lefts: the set of leftward diagonals etc.
+        :param rights: the set of rightward diagonals etc.
+        '''
+        # If we've placed a queen in every row up to now, we've 
+        # found a solution.
+        if row == n:
+            return 1
+        result = 0
+        for column in range(n):
+            # Number the leftward diagonals by the sum of the coordinates.
+            # There will be 2n -1 diagonals.
+            left = row + column
+            # Number the rightward diagonals similarly, but prevent negative values.
+            right = row - column + n
+            if 1 << column & columns or 1 << left & lefts or 1 << right & rights:
+                continue
+            result += internal_queens(row + 1, columns | 1 << column, lefts | 1 << left, rights | 1 << right)
+        return result
+    return internal_queens(0, 0, 0, 0)
+
+    '''
+    Variant of the above, adding memoization to prevent O(n!) performance.
+    '''
+
+    memos = {}
+    hits = 0
+
+    def internal_queens(row: int, columns: int, lefts: int, rights: int) -> int:
+        nonlocal hits
+
+        if row == n:
+            return 1
+
+        key = (columns, lefts, rights)
+        # The row doesn't have to form part of the key. 
+        # For any value of row, all the bitsets will have
+        # the same size (row).
+        if key in memos:
+            hits += 1
+            return memos[key]
+        result = 0
+        for column in range(n):
+            # Number the leftward diagonals by the sum of the coordinates.
+            # There will be 2n -1 diagonals.
+            left = row + column
+            # Number the rightward diagonals similarly, but prevent negative values.
+            right = row - column + n
+            if 1 << column & columns or 1 << left & lefts or 1 << right & rights:
+                continue
+            result += internal_queens(row + 1, columns | 1 << column, lefts | 1 << left, rights | 1 << right)
+        memos[key] = result
+        return result
+    
+    result = internal_queens(0, 0, 0, 0)
+    return result
+
+_KNOWN_SOLUTIONS = [
+    (1, 1),
+    (2, 0),
+    (3, 0),
+    (4, 2),
+    (5, 10),
+    (6, 4),
+    (7, 40),
+    (8, 92),
+    (9, 352),
+    (10, 724)
+]
+
+# It turns out that memoization does not improve the performance, most likely because the 
+# dictionary grows very large. When n = 8, there are 1818 memos, but only 95 cache hits.
+# 
+# Leetcode insists that actual sets use less memory than bitmasks, which baffles me.
+
+@pytest.mark.parametrize("n,solutions", _KNOWN_SOLUTIONS)
+def test_known_solutions(n: int, solutions: int):
+    '''
+    Test against known solutions. Cf. # See https://en.wikipedia.org/wiki/Eight_queens_puzzle
+    '''
+    actual = totalNQueensMemoized(n)
+    assert actual == solutions
+
+    actual = totalNQueensBitmasks(n)
+    assert actual == solutions
+
+@pytest.mark.parametrize("n,solutions", _KNOWN_SOLUTIONS[-3:])
+def test_known_solutions_recursive(benchmark, n: int, solutions: int):
+    benchmark(totalNQueensBitmasks, n)
+
+@pytest.mark.parametrize("n,solutions", _KNOWN_SOLUTIONS[-3:])
+def test_known_solutions_memoized(benchmark, n: int, solutions: int):
+    benchmark(totalNQueensMemoized, n)
