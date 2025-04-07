3d0c091de47048bcdbc1d965cb0be89220a95c08
Tony Nassar <tony@molg.ai>
2025-04-07 15:59:31 -0400

    Starting on new problem

---
+++ leetcode-ruby/spec/valid_number.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/valid-number
+
+# An integer number followed by an optional exponent.
+# A decimal number followed by an optional exponent.
+# An integer number is defined with an optional sign '-' or '+' followed by digits.
+#
+# A decimal number is defined with an optional sign '-' or '+' followed by one of the following definitions:
+#
+# Digits followed by a dot '.'.
+# Digits followed by a dot '.' followed by digits.
+# A dot '.' followed by digits.
+
+def is_number(s)
+  if s =~ /^[eE]\d+$/
+    s =
+  end
+
+end
