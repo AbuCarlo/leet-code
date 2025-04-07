f34ea9c972571a9d12344e8cf8620c0efcfb92c3
Tony Nassar <tony@molg.ai>
2025-04-07 10:07:33 -0400

    Solved!

--- leetcode-python/test_substrings_concatenated.py
+++ leetcode-python/test_substrings_concatenated.py
@@ -11,15 +11,19 @@ import pytest
 import hypothesis
 from hypothesis import strategies
 
-class ZeroingCount(collections.Counter):
+class ZeroingCounter(collections.Counter):
     '''
     This class encapsulates the logic of a sliding count. If any count
-    becomes 0, the key is deleted. An empty counter means that the current
-    window contains a permutation of the input tokens.
+    becomes 0, the key is deleted. We initialize the counter from a list 
+    of search tokens. If all values (counts) become 0, the current window
+    contains a permutation of the input tokens (i.e.). A negative value
+    means that the current keyb (token) appears too many times in the 
+    current window.
     '''
 
-    # We don't need to override the constructor, since the initial
-    # values will not include 0s.
+    # We don't need to override the constructor since the initial
+    # values will not include 0s. update() should be overriden, but
+    # we never invoke it.
 
     def __setitem__(self, key, value):
         if value == 0:
@@ -37,6 +41,10 @@ def find_concatenations(s: str, tokens: list[str]) -> int:
     '''
 
     def find_all_overlapping(token: str) -> typing.Iterator[int]:
+        '''
+        Find all over instances of a token in the input string,
+        possibly overlapping.
+        '''
         start = 0
         while True:
             start = s.find(token, start)
@@ -45,13 +53,14 @@ def find_concatenations(s: str, tokens: list[str]) -> int:
             yield start
             start += 1
 
-    # All the tokens are the same length.
     token_set = set(tokens)
     # Several test cases time out when there's only 1 token value, 
     # esp. a very short one.
     if len(token_set) == 1:
         token = tokens[0] * len(tokens)
         return list(find_all_overlapping(token))
+
+    # All the tokens are the same length.
     token_length = len(tokens[0])
     # We may be given only two tokens, and they might be equal.
     # So there's not much point in looking for the best token.
@@ -60,24 +69,30 @@ def find_concatenations(s: str, tokens: list[str]) -> int:
 
     all_results = []
 
+    # Matches may start at any index. A match at i will be a concatenation
+    # tokens, all the same length.  
     for remainder in set(p % token_length for p in anchor_positions):
 
         results = []
-        # Slicing up the input is easier than lots of finicky substring matching. Get rid
-        # of terminal tokens that are too short to match anyway.
-        sliced = [s[i:i + token_length] for i in range(remainder, len(s), token_length) if len(s) - i >= token_length]
+        # Slicing up the input is easier than lots of finicky substring matching, and in 
+        # any case, we have to create substrings as keys, so let's just slice up the input.
+        sliced = [s[i:i + token_length] for i in range(remainder, len(s), token_length)]
 
+        # the index of the slice representing the (possible) rightmost slice of a match
         r = 0
 
-        counts = ZeroingCount(tokens)
-
+        counts = ZeroingCounter(tokens)
+        # the left of a potential match, and the current token
         for l, sl in enumerate(sliced):
             if sl not in token_set:
+                # Advance the right index past the current token.
                 r = l + 1
-                counts = ZeroingCount(tokens)
+                # Reset the counts.
+                counts = ZeroingCounter(tokens)
                 continue
-
+            # Check off this token.
             counts[sl] -= 1
+            # Discard the rightmost token if we have too many.
             if l - r == len(tokens):
                 counts[sliced[r]] += 1
                 r += 1
@@ -117,19 +132,6 @@ def test_samples(s: str, tokens: list[str], expected: int):
     actual = find_concatenations(s, tokens)
     assert actual == expected
 
-
-@strategies.composite
-def add_whitespace(draw):
-    '''
-    A match should be able to begin anywhere in the input string.
-    '''
-    s, tokens, expected = draw(strategies.sampled_from(_SAMPLES))
-    prefix_length = draw(strategies.integers(min_value=0, max_value=len(tokens[0])))
-    suffix_length = draw(strategies.integers(min_value=0, max_value=len(tokens[0])))
-    s = (' ' * prefix_length) + s + (' ' * suffix_length)
-    return (s, tokens, expected)
-
-
 @strategies.composite
 def permute_tokens(draw):
     '''
@@ -138,16 +140,6 @@ def permute_tokens(draw):
     _, tokens, _ = draw(strategies.sampled_from(_SAMPLES))
     permutation = draw(strategies.permutations(tokens))
     return (''.join(permutation), tokens)
-
-
-@hypothesis.given(add_whitespace())
-# pylint: disable=C0116
-def test_with_whitespace(t):
-    s, tokens, expected = t
-    actual = find_concatenations(s, tokens)
-    assert actual == expected
-
-# TODO: Modify to just take whitespace as argument.
 
 @hypothesis.given(permute_tokens())
 # pylint: disable=C0116
