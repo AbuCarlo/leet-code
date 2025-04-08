9fed92facc62091e973f0687d33141b7ae29d179
Tony Nassar <tony@molg.ai>
2025-04-08 10:33:49 -0400

    Solved!

--- leetcode-ruby/spec/valid_number.rb
+++ leetcode-ruby/spec/valid_number.rb
@@ -1,5 +1,12 @@
 # https://leetcode.com/problems/valid-number
-
+# "Given a string s, return whether s is a valid number.
+#
+# For example, all the following are valid numbers: "2", "0089", "-0.1", "+3.14",
+# "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789",
+# while the following are not valid numbers: "abc", "1a", "1e", "e3", "99e2.5",
+# "--6", "-+3", "95a54e53".
+# Formally, a valid number is defined using one of the following definitions:
+#
 # An integer number followed by an optional exponent.
 # A decimal number followed by an optional exponent.
 # An integer number is defined with an optional sign '-' or '+' followed by digits.
@@ -11,11 +18,12 @@
 # A dot '.' followed by digits.
 
 def is_number(s)
-  exponent = s =~ /e\d+\z/i
+  # Can an exponent have leading 0s?
+  exponent = s =~ /e[+-]?\d+\z/i
   if exponent
     s = s[0...exponent]
   end
-
-  is_integer(s) || is_decimal(s)
+  # It turns out that nothing is faster than just using a regex.
+  !(s =~ /^[+-]?((\d+)(\.\d*)?|(\.\d+))$/).nil?
 
 end
