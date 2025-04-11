6a6e166eb8d9e3df4f8c5284a2b619915ea20f96
Tony Nassar <tony@molg.ai>
2025-04-11 16:05:06 -0400

    Solved, but slow.

--- leetcode-python/test_three_sum_closest.py
+++ leetcode-python/test_three_sum_closest.py
@@ -21,19 +21,25 @@ def three_sum_closest(a: list[int], target: int) -> int:
 
     def test_m(l, m, r):
         nonlocal result
+        assert m > l and m < r
         t = a[l] + a[m] + a[r]
         if abs(t - target) < abs(result - target):
             result = t
 
+    
     for l in range(len(a) - 2):
-        for r in range(l + 2, len(a)):
+        for r in range(len(a) - 1, l + 1, -1):
             t = target - a[l] - a[r]
-            m = bisect.bisect_left(a, t, l + 1, r)
+            m = bisect.bisect_left(a, t, l + 1, r - 1)
             # See the documention for bisect.
-            if m > l:
-                test_m(l, m, r)
+            if a[m] == t:
+                return target
+            if m - 1 > l:
+                test_m(l, m - 1, r)
             if m < r:
                 test_m(l, m, r)
+            if m + 1 < r:
+                test_m(l, m + 1, r)
 
     return result
 
@@ -43,7 +49,11 @@ _SAMPLES = [
     # test case 29
     ([1, 1, -1], 2, 1),
     # test case 63
-    ([10, 20, 30, 40, 50, 60, 70, 80, 90], 1, 60)
+    ([10, 20, 30, 40, 50, 60, 70, 80, 90], 1, 60),
+    # test case 105
+    ([2, 5, 6, 7], 16, 15),
+    # test case 70
+    ([1, 1, 1, 0], 100, 3)
 ]
 
 @pytest.mark.parametrize("a,target,expected", _SAMPLES)
