9d13e409138b01658c51d9e3ce99c0f918733d6b
Tony Nassar <tony@molg.ai>
2025-05-23 15:49:14 -0400

    WIP on custom BST

--- leetcode-java/src/test/java/com/aanassar/study/leetcode/TestCountGoodTriplets.java
+++ leetcode-java/src/test/java/com/aanassar/study/leetcode/TestCountGoodTriplets.java
@@ -8,6 +8,7 @@ import org.junit.runners.Parameterized;
 import java.util.*;
 import java.util.stream.IntStream;
 
+
 /**
  * See <a href="https://leetcode.com/problems/count-good-triplets-in-an-array/description/">...</a>
  * <p>
@@ -20,6 +21,56 @@ import java.util.stream.IntStream;
  */
 @RunWith(Parameterized.class)
 public class TestCountGoodTriplets {
+
+    record LesserGreater(int lesser, int greater) {}
+    
+    static class CustomTree {
+        private final int[] lessers;
+        private final int[] greaters;
+        private final int root;
+
+        CustomTree(int size) {
+            int powerOfTwo = 1;
+            do {
+                powerOfTwo <<= 1;
+            } while (powerOfTwo - 1 < size);
+            this.lessers = new int[powerOfTwo - 1];
+            this.greaters = new int[powerOfTwo - 1];
+            this.root = (powerOfTwo >> 1) - 1;
+        }
+        
+        void add(int n) {
+            assert n < lessers.length;
+            int width = (this.root + 1) / 2;
+            int i = this.root;
+            while (i != n) {
+                if (n < i) {
+                    ++this.lessers[i];
+                    i -= width;
+                } else {
+                    ++this.greaters[i];
+                    i += width;
+                }
+                width >>= 1;
+            }
+        }
+
+        LesserGreater find(int n) {
+            assert n < lessers.length;
+            int width = (this.root + 1) / 2;
+            int i = this.root;
+            while (i != n) {
+                if (n < i) {
+                    i -= width;
+                } else {
+                    i += width;
+                }
+                width >>= 1;
+            }
+            return new LesserGreater(this.lessers[i], this.greaters[i]);
+        }
+    }
+    
     public long goodTriplets(int[] l, int[] r) {
         // There is no neat way to do this without Guava 21.
         // To give credit where credit is due: https://stackoverflow.com/a/18552071/476942
@@ -31,20 +82,19 @@ public class TestCountGoodTriplets {
                 .mapToInt(t -> t[1])
                 .toArray();
         // We know have an array mapping the values in l to their positions.
-        var tree = new TreeSet<Integer>();
+        var tree = new CustomTree(l.length);
         long result = 0L;
         // Now for each value in r, determine how many values preceding it *also*
         // preceded it in l.
         for (int j : r) {
             // Where was this value in l?
             int indexInLeft = withIndices[j];
+            var tuple = tree.find(indexInLeft);
             // How many smaller indices into l are already in the tree?
-            long smaller = tree.headSet(indexInLeft).size();
             // How many larger indices are already in the tree? These cannot
             // be the third element of a good triplet. How many larger indices
             // are left over?
-            long larger = tree.tailSet(indexInLeft).size();
-            long tripletsWithThisValue = smaller * (r.length - indexInLeft - 1 - larger);
+            long tripletsWithThisValue = (long) tuple.lesser * (r.length - indexInLeft - 1 - tuple.greater);
             result += tripletsWithThisValue;
             tree.add(indexInLeft);
         }
