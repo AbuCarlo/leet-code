120667342fc2b3136c090dd88eed72fd62ce893b
Tony Nassar <anthonyabunassar@gmail.com>
2025-07-05 11:32:00 -0400

    Populating first data structure

--- leetcode-python/test_trap_rain_water_ii.py
+++ leetcode-python/test_trap_rain_water_ii.py
@@ -4,42 +4,9 @@ Extend the infamous interview question to 3D.
 https://leetcode.com/problems/trapping-rain-water-ii
 '''
 
-from dataclasses import dataclass
 from typing import List
 
 import pytest
-
-@dataclass
-class RainWater:
-    """Class for keeping track of an item in inventory."""
-    height: int
-    left: int
-    right: int
-
-def trap_across(l: List[int]) -> List[RainWater]:
-    '''
-    Calculate the trapped rainwater in 2 dimensions,
-    noting the indices of the left and right maxima.
-    '''
-    current = (0, -1)
-    from_left = [None] * len(l)
-    for i, h in enumerate(l):
-        if h >= current[0]:
-            current = (h, i)
-        from_left[i] = current
-    current = (0, len(l))
-    from_right = [None] * len(l)
-    for i in range(len(l) - 1, -1, -1):
-        h = l[i]
-        if h >= current[0]:
-            current = (h, i)
-        from_right[i] = current
-    traps = []
-    for left, right in zip(from_left, from_right):
-        r = RainWater(min(left[0], right[0]), left[1], right[1])
-        traps.append(r)
-    return traps
-
 
 # pylint: disable=C0103,C0200
 def trapRainWater(heights: List[List[int]]) -> int:
@@ -47,53 +14,32 @@ def trapRainWater(heights: List[List[int]]) -> int:
     Try it.
     '''
     assert all(len(l) == len(heights[0]) for l in heights)
-    across = [trap_across(l) for l in heights]
-    down = [[0] * len(l) for l in heights]
-    for column in range(len(heights[0])):
-        from_top = [0] * len(heights)
-        upper_maximum =0
-        for row in range(len(heights)):
-            h = heights[row][column]
-            upper_maximum = max(h, upper_maximum)
-            from_top[row] = upper_maximum
-        lower_maximum = 0
-        for row in range(len(heights) - 1, -1, -1):
-            h = heights[row][column]
-            lower_maximum = max(lower_maximum, h)
-            down[row][column] = min(lower_maximum, from_top[row])
-    both = [[0] * len(l) for l in heights]
-    for row, a in enumerate(down):
-        for column, h in enumerate(a):
-            trap = across[row][column]
-            current = h
-            for i in range(trap.left, trap.right + 1):
-                current = min(current, down[row][i])
-            both[row][column] = current
-    result = 0
-    for row, l in enumerate(heights):
-        for column, h in enumerate(l):
-            water = both[row][column] - h
-            if water > 0:
-                result += water
-    return result
+    h = len(heights)
+    w = len(heights[0])
+    limit = max(h, w)
+
+    block_nw = [[0 for _ in range(w)] for _ in range(h)]
+    block_nw[0][0] = heights[0][0]
+    # Move diagonally, in a SE direction.
+    for d in range(1, limit):
+        # Fill in the first row.
+        for c in range(w):
+            block_nw[0][c] = heights[0][c]
+        # Fill in remaining rows.
+        for r in range(min(d, h - 1), 0, -1):
+            # Fill in column 0.
+            block_nw[r][0] = heights[r][0]
+            # Fill in the values moving in a NE direction.
+            for c in range(1, min(d + 1, w)):
+                block_nw[r][c] = max(heights[r][c], min(block_nw[r][c - 1], block_nw[r - 1][c]))
+
+    return 0
 
 
 _SAMPLES_2D = [
     ([3,3,3,3,3], 0),
     ([3,2,2,2,3], 3)
 ]
-
-
-# pylint: disable=C0116
-@pytest.mark.parametrize("l, expected", _SAMPLES_2D)
-def test_2d_samples(l, expected):
-    '''
-    Confirm that this implementation works in 2D, like the "classic."
-    '''
-    traps = trap_across(l)
-    actual = sum(max(0, traps[i].height - h) for i, h in enumerate(l))
-    assert actual == expected
-
 
 # pylint: disable=C0116
 @pytest.mark.parametrize("l, _", _SAMPLES_2D)
