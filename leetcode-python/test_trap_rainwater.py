a5ddb4c7929cd39b72cda0025b5640edc57d0661
Tony Nassar <anthonyabunassar@gmail.com>
2025-12-14 07:46:24 -0500

    Code cleanup.

--- leetcode-python/test_trap_rainwater.py
+++ leetcode-python/test_trap_rainwater.py
@@ -45,24 +45,25 @@ def trap_rainwater_faster(height: List[int]) -> int:
     r = len(height) - 2
     while l <= r:
         if left_max <= right_max:
-            
-            if height[l] >= left_max:
+            if height[l] > left_max:
                 left_max = height[l]
             else:
                 result += left_max - height[l]
             l += 1
         else:
-            if height[r] >= right_max:
+            if height[r] > right_max:
                 right_max = height[r]
             else:
                 result += right_max - height[r]
             r -= 1
     return result
 
+
 _SAMPLES_2D = [
     ([0,1,0,2,1,0,1,3,2,1,2,1], 6),
     ([4,2,0,3,2,5], 9),
 ]
+
 
 def trap_rainwater_with_heap(height: List[int]) -> int:
     '''
